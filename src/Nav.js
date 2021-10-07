e9f457c7a643adf6696ab5d1def884d262255a68
Charles <crg0905@gmail.com>
2021-10-07 10:34:14 -0700

    add Netflix logo & profile img

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,9 @@ import "./Nav.css"
 
 function Nav() {
     return <div className="nav">
-            
+        <img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" alt="Netflix Logo"  />
+
+        <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="text" />
         </div>
 }
 
