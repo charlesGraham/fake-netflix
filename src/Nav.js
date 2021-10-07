5f858dcd36679d931f08ab304231e18b2aaf0e06
Charles <crg0905@gmail.com>
2021-10-07 10:42:51 -0700

    image styles

--- src/Nav.js
+++ src/Nav.js
@@ -2,11 +2,25 @@ import React from "react";
 import "./Nav.css"
 
 function Nav() {
-    return <div className="nav">
-        <img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" alt="Netflix Logo"  />
+    return (
+        <div className="nav_contents">
+            <div className="nav">
+                <img 
+                    className="nav_logo" 
+                    src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" 
+                    alt="Netflix Logo" 
+                />
 
-        <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="text" />
+                <img
+                    className="nav_avatar" 
+                    src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" 
+                    alt="text" 
+                />
+            </div>
         </div>
+        
+    )
+    
 }
 
 export default Nav;
