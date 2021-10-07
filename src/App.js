9dbc95f7d377dbae513aea5abcbca32672d59548
Charles <crg0905@gmail.com>
2021-10-07 10:12:27 -0700

    move counterSlice and remove svg logo import from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>let's build netflix!</h1>
     </div>
   );
