bd222b0a4435f732ee5f9220347f1104b53ef2fe
Charles <crg0905@gmail.com>
2021-10-07 14:43:57 -0700

    implementing TMDB api

--- src/Banner.js
+++ src/Banner.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './Banner.css';
 
 function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substring(0, n-1) + "..." : string;
+    }
+
     return (
         <header
             className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+            backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
             backgroundPosition: "center center",
         }}>
 
@@ -16,7 +21,9 @@ function Banner() {
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
                 </div>
-                <h1 className="banner_description">This is a test description</h1>
+                <h1 className="banner_description">
+                {truncate(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`, 150)}
+                </h1>
             </div>
 
             <div className="banner--fadeBottom" />
