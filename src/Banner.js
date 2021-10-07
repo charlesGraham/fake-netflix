47f54f80217f4466ed9c2ab8c38aeb84155e9b35
Charles <crg0905@gmail.com>
2021-10-07 13:52:07 -0700

    add banner & styles

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './Banner.css';
 
 function Banner() {
     return (
-        <div>
-            
-        </div>
+        <header className="banner" style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+            backgroundPosition: "center center",
+        }}>
+
+
+
+        </header>
     )
 }
 
-export default Banner
+export default Banner;
