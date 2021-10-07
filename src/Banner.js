4497c2cfa9032581fc2cfaca70be5200eed33519
Charles <crg0905@gmail.com>
2021-10-07 13:57:22 -0700

    banner styles + removing defaults

--- src/Banner.js
+++ src/Banner.js
@@ -3,11 +3,23 @@ import './Banner.css';
 
 function Banner() {
     return (
-        <header className="banner" style={{
+        <header
+            className="banner" style={{
             backgroundSize: "cover",
             backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
             backgroundPosition: "center center",
         }}>
+
+            <div className="banner_contents">
+                <h1 className="banner_title">Movie Name</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">This is a test description</h1>
+            </div>
+
+            <div className="banner--fadeBottom" />
 
 
 
