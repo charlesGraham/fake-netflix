790b38d1624296307e17e9a36161b75ffec6f42b
Charles <crg0905@gmail.com>
2021-10-07 16:19:43 -0700

    create Row components to render different requests

---
+++ src/Row.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import "./Row.css";
+
+function Row() {
+    return (
+        <div className="row">
+            <h2>This is a title</h2>
+        </div>
+    )
+}
+
+export default Row;
