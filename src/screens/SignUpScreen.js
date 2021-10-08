cc46c8f2f120e0ab7137e6f3af6effb66ed1c399
Charles <crg0905@gmail.com>
2021-10-08 10:56:52 -0700

    SignUpScreen styles

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -4,7 +4,12 @@ import "./SignUpScreen.css";
 function SignUpScreen() {
     return (
         <div className="signupScreen">
-            
+            <form>
+                <h1>Sign In</h1>
+                <input placeholder="Email" type="email" />
+                <input placeholder="Password" type="password" />
+                <button type="submit">Sign In</button>
+            </form>
         </div>
     )
 }
