56eaeced4d10cfaf2915c362be4131fe79e7a0e7
Charles <crg0905@gmail.com>
2021-10-08 13:40:24 -0700

    finish frontend of the SignUpScreen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -9,6 +9,11 @@ function SignUpScreen() {
                 <input placeholder="Email" type="email" />
                 <input placeholder="Password" type="password" />
                 <button type="submit">Sign In</button>
+
+                <h4>
+                    <span className="signupScreen_gray">New to Netflix? </span> 
+                    <span className="signupScreen_link">Sign Up now.</span>
+                </h4>
             </form>
         </div>
     )
