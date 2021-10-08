c9b7983facf1d7eb6963b89a540a01e170800055
Charles <crg0905@gmail.com>
2021-10-08 09:22:55 -0700

    implement React Router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React from 'react';
 // import { Counter } from './features/counter/Counter';
 import './App.css';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      
+
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+    </Router>
+
+
+
     </div>
   );
 }
