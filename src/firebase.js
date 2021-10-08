e1fb1adf0973110708583b1739cb7bceb427e974
Charles <crg0905@gmail.com>
2021-10-07 21:09:14 -0700

    firebase additions

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
     apiKey: "AIzaSyB0kw_PlGM1ioSqgp9sSZcvpEYCaUaRc6I",
     authDomain: "netflix-clone-45593.firebaseapp.com",
@@ -6,3 +8,5 @@ const firebaseConfig = {
     messagingSenderId: "929696350404",
     appId: "1:929696350404:web:4d452d1359562e1008c991"
   };
+
+  const app = initializeApp(firebaseConfig);
