1c2922589d5a7aa1193d445595d8be502daa5c72
Charles <crg0905@gmail.com>
2021-10-08 10:48:22 -0700

    create SignUpScreen component. conditionally render if Get Started is pressed

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./LoginScreen.css";
+import SignUpScreen from './SignUpScreen';
 
 function LoginScreen() {
+    const [signIn, setSignIn] = useState(false);
+
     return (
         <div className="loginScreen">
             <div className="loginScreen_background">
@@ -10,8 +13,8 @@ function LoginScreen() {
                 src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
                 alt="logo" />
                 <button 
+                    onClick={() => setSignIn(true)}
                     className="loginScreen_button"
-
                 >
                     Sign In
                 </button>
@@ -19,22 +22,29 @@ function LoginScreen() {
             </div>
 
             <div className="loginScreen_body">
-                <>
-                    <h1>Unlimited films, TV programs and more.</h1>
-                    <h2>Watch anywhere. Cancel at any time.</h2>
-                    <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-                    <div className="loginScreen_input">
-                        <form>
-                            <input 
-                                type="email"
-                                placeholder="Email Address" 
-                            />
-                            <button className="loginScreen_getStarted">
-                                GET STARTED
-                            </button>
-                        </form>
-                    </div>
-                </>
+                {signIn ? (
+                    <SignUpScreen />
+                ) : (
+                        <>
+                            <h1>Unlimited films, TV programs and more.</h1>
+                            <h2>Watch anywhere. Cancel at any time.</h2>
+                            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                            <div className="loginScreen_input">
+                                <form>
+                                    <input 
+                                        type="email"
+                                        placeholder="Email Address" 
+                                    />
+                                    <button
+                                        onClick={() => setSignIn(true)}
+                                        className="loginScreen_getStarted">
+                                        GET STARTED
+                                    </button>
+                                </form>
+                            </div>
+                        </>
+                )}
+                
             </div>
         </div>
     )
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SignUpScreen.css";
+
+function SignUpScreen() {
+    return (
+        <div className="signupScreen">
+            
+        </div>
+    )
+}
+
+export default SignUpScreen;
