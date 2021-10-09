d4d6bad6bdaaee8f0a832aac74c38d476c657476
Charles <crg0905@gmail.com>
2021-10-08 21:19:34 -0700

    Netflix logo navigates to the HomeScreen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Nav from '../Nav';
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
     return (
         <div className="profileScreen">
-            <h1>this is the profile screen</h1>
+            <Nav />
         </div>
     )
 }
