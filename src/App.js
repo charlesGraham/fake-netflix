d31bdfc511e4c8ecadd9400be3d00b238391d88b
Charles <crg0905@gmail.com>
2021-10-08 20:53:16 -0700

    finish auth implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import { Counter } from './features/counter/Counter';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
@@ -8,9 +8,22 @@ import {
   Route,
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+      } else {
+        //logged out
+      }
+    });
+
+    return unsubscribe;
+  }, [])
 
   return (
     <div className="app">
