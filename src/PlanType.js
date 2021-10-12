106cac7ec4c3742078b2f83be759fef18455a8f5
Charles <crg0905@gmail.com>
2021-10-12 16:39:02 -0700

    create PlanType component and render plan types on ProfileScreen

---
+++ src/PlanType.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./PlanType.css";
+
+function PlanType(props) {
+    return (
+        <div className="profileScreen_planType">
+            <div className="profileScreen_planInfo">
+                <h3>Netflix {props.name}</h3>
+                <p>{props.hd}</p>
+            </div>
+            <button 
+                className={(props.sub === false) ? "notSubbed" : "subbed"}
+            >
+                {(props.sub === false) ? "Subscribe" : "Current Package"}
+            </button>
+        </div>
+    )
+}
+
+export default PlanType
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,6 +4,7 @@ import { selectUser } from '../features/userSlice';
 import Nav from '../Nav';
 import { auth } from "../firebase";
 import "./ProfileScreen.css";
+import PlanType from '../PlanType';
 
 function ProfileScreen() {
     const user = useSelector(selectUser);
@@ -22,7 +23,10 @@ function ProfileScreen() {
                         <h2>{user.email}</h2>
                         <div className="profileScreen_plans">
                             <h3>Plans</h3>
-                            <p></p>
+                            <p>Renewal Date: 10/12/2022</p>
+                            <PlanType name={"Standard"} hd={"1080p"} sub={false} />
+                            <PlanType name={"Basic"} hd={"720p"} sub={false} />
+                            <PlanType name={"Premium"} hd={"4K + HDR"} sub={true} />
                             <button
                                 onClick={() => auth.signOut()} 
                                 className="profileScreen_signOut">
